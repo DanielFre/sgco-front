5bf2d86ce9f3565f31efd0def879c50f1b966ef9
Daniel <daniel._.frey@hotmail.com>
2019-09-04 22:00:31 -0300

    modifcs

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,12 +8,12 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+    { path: '/dashboard', title: 'Tela Principal',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Procedimentos',  icon:'person', class: '' },
+    { path: '/table-list', title: 'Agenda',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Funcionários',  icon:'library_books', class: '' },
+    { path: '/icons', title: 'Financeiro',  icon:'bubble_chart', class: '' },
+    { path: '/maps', title: 'Pacientes',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
