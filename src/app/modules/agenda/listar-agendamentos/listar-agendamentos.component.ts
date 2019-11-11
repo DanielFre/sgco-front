722ac1b7f4833663aadc1e31b5086aeeae7cb428
Wiliam Felber <wiliamfelber@hotmail.com>
2019-11-11 15:41:02 -0300

    Adicionado FullCalendar
    Deem um npm install para pegar as novas dependecias

--- src/app/modules/agenda/listar-agendamentos/listar-agendamentos.component.ts
+++ src/app/modules/agenda/listar-agendamentos/listar-agendamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-listar-agendamentos',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarAgendamentosComponent implements OnInit {
 
+  calendarPlugins = [dayGridPlugin]; // important!
+
+
+  calendarEvents = [
+    { title: 'event 1', date: '2019-04-01' }
+  ];
+
+  addEvent() {
+    this.calendarEvents = this.calendarEvents.concat({ // creates a new array!
+      title: 'event 2', date: '2019-04-02'
+    }
+    );
+  }
+
+  modifyTitle(eventIndex, newTitle) {
+    let calendarEvents = this.calendarEvents.slice(); // a clone
+    let singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
+    singleEvent.title = newTitle;
+    calendarEvents[eventIndex] = singleEvent;
+    this.calendarEvents = calendarEvents; // reassign the array
+  }
+  
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
