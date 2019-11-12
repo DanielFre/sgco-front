dc26ea69519b43048c2fc4791ac1ce743e1f69e1
Wiliam Felber <wiliamfelber@hotmail.com>
2019-11-12 17:20:26 -0300

    Corrigido um problema no FullCalendar

--- src/app/modules/agenda/listar-agendamentos/listar-agendamentos.component.ts
+++ src/app/modules/agenda/listar-agendamentos/listar-agendamentos.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { EventInput } from '@fullcalendar/core';
+import { EventInput, Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
-
 
 @Component({
   selector: 'app-listar-agendamentos',
@@ -19,7 +18,6 @@ export class ListarAgendamentosComponent implements OnInit {
     { title: 'Event Now', start: new Date() }
   ];
 
-
   handleDateClick(arg) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
       this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
@@ -29,8 +27,6 @@ export class ListarAgendamentosComponent implements OnInit {
       })
     }
   }
-
-
 
   constructor() { }
 
